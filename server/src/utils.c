c3f9c6d5956eb442807b7f0d46cf12089b4ed2da
BrunoBarlari <bruno2004b@gmail.com>
2024-03-27 00:58:31 +0000

    Bind socket server

--- server/src/utils.c
+++ server/src/utils.c
@@ -1,6 +1,6 @@
-#include"utils.h"
+#include "utils.h"
 
-t_log* logger;
+t_log *logger;
 
 int iniciar_servidor(void)
 {
@@ -20,15 +20,16 @@ int iniciar_servidor(void)
 
 	socket_servidor = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
-	if (socket_servidor == -1) {
+	if (socket_servidor == -1)
+	{
 		log_error(logger, "Error al crear el socket");
 		abort();
 	}
 
-
-	//TODO: Asociamos el socket a un puerto
-	bind();
-	//TODO: Escuchamos las conexiones entrantes
+	// TODO: Asociamos el socket a un puerto
+	bind(sockect_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+	
+	// TODO: Escuchamos las conexiones entrantes
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -51,7 +52,7 @@ int esperar_cliente(int socket_servidor)
 int recibir_operacion(int socket_cliente)
 {
 	int cod_op;
-	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+	if (recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
 		return cod_op;
 	else
 	{
@@ -60,9 +61,9 @@ int recibir_operacion(int socket_cliente)
 	}
 }
 
-void* recibir_buffer(int* size, int socket_cliente)
+void *recibir_buffer(int *size, int socket_cliente)
 {
-	void * buffer;
+	void *buffer;
 
 	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
 	buffer = malloc(*size);
@@ -74,27 +75,27 @@ void* recibir_buffer(int* size, int socket_cliente)
 void recibir_mensaje(int socket_cliente)
 {
 	int size;
-	char* buffer = recibir_buffer(&size, socket_cliente);
+	char *buffer = recibir_buffer(&size, socket_cliente);
 	log_info(logger, "Me llego el mensaje %s", buffer);
 	free(buffer);
 }
 
-t_list* recibir_paquete(int socket_cliente)
+t_list *recibir_paquete(int socket_cliente)
 {
 	int size;
 	int desplazamiento = 0;
-	void * buffer;
-	t_list* valores = list_create();
+	void *buffer;
+	t_list *valores = list_create();
 	int tamanio;
 
 	buffer = recibir_buffer(&size, socket_cliente);
-	while(desplazamiento < size)
+	while (desplazamiento < size)
 	{
 		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
-		desplazamiento+=sizeof(int);
-		char* valor = malloc(tamanio);
-		memcpy(valor, buffer+desplazamiento, tamanio);
-		desplazamiento+=tamanio;
+		desplazamiento += sizeof(int);
+		char *valor = malloc(tamanio);
+		memcpy(valor, buffer + desplazamiento, tamanio);
+		desplazamiento += tamanio;
 		list_add(valores, valor);
 	}
 	free(buffer);
